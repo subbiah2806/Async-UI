6b3052af2be6e8f89b2930bf342fe0395851692f
schandramouli2 <subbiah_chandramouli@apple.com>
2019-05-19 11:12:10 -0700

    gh-pages baseUrl

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './routes/Home';
 class Index extends React.Component {
 	render() {
+		const baseUrl = process.env.PUBLIC_URL;
 		return (
 			<BrowserRouter>
-				<Route exact path="/" component={Home}/>
+				<Route exact path={`${baseUrl}/`} component={Home}/>
 			</BrowserRouter>
 		)
 	}
