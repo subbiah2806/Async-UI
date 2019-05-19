ed7712866be8a017043fc2e82f9886336daa529a
schandramouli2 <subbiah_chandramouli@apple.com>
2019-05-19 00:55:35 -0700

    footer

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.scss';
 import { CustomSvg, CircleIcon } from '../components';
 import Avatar from '@material-ui/core/Avatar';
+import Fab from '@material-ui/core/Fab';
 import space from '../assets/space.jpg';
 import dp from '../assets/dp.jpg';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 const useitlate = "I build websites that don't fly.";
 
-const iconColors = deepPurple[300];
+const iconColor = deepPurple[300];
+const HomeFooterColor = deepPurple['800'];
 var bg = {
 	backgroundImage: `url(${space})`,
 };
@@ -26,30 +28,43 @@ class Home extends React.Component {
 		return (
 			<div id="Home">
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-				<div style={bg} className="bg">
+				<section style={bg} className="section1">
 					<div className="content">
 						<div className="content-margin">
 							<h1>I am Subbiah</h1>
 							<h2>Web Developer</h2>
 						</div>
 					</div>
-				</div>
-				<div className="section">
-					<Avatar alt="dp" src={dp} className="dp" />
-
-
-					<footer className="footer">
+				</section>
+				<section className="section2">
+					<Avatar alt="Display Picture" src={dp} className="dp"/>
+					<footer className="section2-footer">
 						<CircleIcon text="I build websites, my experience builds my resume">
-							<CustomSvg svgName="linkedIn" width="50" height="50" style={{fill: iconColors}}/>
+							<CustomSvg svgName="linkedIn" width="50" height="50" style={{fill: iconColor}}/>
 						</CircleIcon>
 						<CircleIcon text="I write code that doesn't bite.">
-							<CustomSvg svgName="github" width="60" height="60" style={{fill: iconColors}}/>
+							<CustomSvg svgName="github" width="60" height="60" style={{fill: iconColor}}/>
 						</CircleIcon>
 						<CircleIcon text="I do things that I love the most.">
-							<CustomSvg svgName="facebook" width="47" height="47" style={{fill: iconColors}}/>
+							<CustomSvg svgName="facebook" width="47" height="47" style={{fill: iconColor}}/>
 						</CircleIcon>
 					</footer>
-				</div>
+				</section>
+				<footer className="Home-footer" style={{background: HomeFooterColor}}>
+					<Avatar alt="Display Picture" src={dp} className="dp"/>
+					<div className="line" />
+					<a className="email" href="mailto:subbiah2806@gmail.com?Subject=Regarding%20web%20development" target="_top">
+						<h2>subbiah2806@gmail.com</h2>
+					</a>
+					<div className="buttons">
+						<div className="buttons-left">
+							<Fab variant="extended" size="medium" component="button" aria-label="Add"> LinkedIn</Fab>
+							<Fab variant="extended" size="medium" component="button" aria-label="Add"> Github</Fab>
+							<Fab variant="extended" size="medium" component="button" aria-label="Add"> Facebook</Fab>
+						</div>
+						<p>Web developer based on <br/> Bay area, california</p>
+					</div>
+				</footer>
 			</div>
 		)
 	}
