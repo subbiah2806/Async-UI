f57d29c8b8879dab3ca7a3f92e5a3ee7303d3910
schandramouli2 <subbiah_chandramouli@apple.com>
2019-07-20 18:08:06 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19583770c40a0f7242b6a2dcf066d58c.js"
+  "/precache-manifest.15c7ba28c09a8a6fa4b13f128913fffd.js"
 );
 
 self.addEventListener('message', (event) => {
